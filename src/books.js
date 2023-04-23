5e2136eb537c672bad8a56a82494f0501501f1a1
meysdays <igbokwevictor122@gmail.com>
2023-04-23 23:55:35 +0100

    Book-List using React Component

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+    author: 'Peter Attia MD ',
+    title: 'Outlive',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Don Miguel Ruiz ',
+    title: 'The Four Agreements',
+    img: img3,
+    id: 3,
+  },
+]
